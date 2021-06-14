462abb93c790f82d281c97595598ad07db90d46f
walleser-karli <k.walleser@ufl.edu>
2021-06-13 20:01:00 -0400

    Add click events to each box with new message

--- main.js
+++ main.js
@@ -13,3 +13,9 @@ for(i=0; i<boxes.length; i++){
     boxes[i].addEventListener("mouseenter", function(){ document.getElementsByClassName("col-sm-4").style.backgroundColor = "white"; });
     boxes[i].addEventListener("mouseleave", function(){ document.getElementsByClassName("col-sm-4").style.backgroundColor = "black"; });
     }
+
+boxes[0].addEventListener("click", function(){ alert("Oooh - so close, but no cigar");})
+
+boxes[1].addEventListener("click", function(){ alert("DING DING DING - We have a winner");})
+
+boxes[2].addEventListener("click", function(){ alert("Oops, butter luck next time");})
