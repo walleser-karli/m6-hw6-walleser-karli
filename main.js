298150a7d052d20ab9e9ee925211311fa678ded1
walleser-karli <k.walleser@ufl.edu>
2021-06-17 20:18:36 -0400

    Fix for loop to change the colors of all boxes when you hover over one

--- main.js
+++ main.js
@@ -1,41 +1,16 @@
 var boxes = document.getElementsByClassName("col-sm-4");
 console.log(boxes);
 
-// document.getElementsByClassName("col-sm-4").addEventListener("mouseover", function() {
-//     boxes[0].classList.toggle("box-white");
-// });
-
-// boxes[0].addEventListener("mouseenter",
-//     function () {
-//         var colorChange = document.getElementsByClassName("col-sm-4");
-//         colorChange.classList.toggle("box-white");
-//     }
-// );
-
 function colorChange () {
-    boxes[0].classList.toggle("box-white");
+    for(i=0; i<boxes.length; i++){
+       boxes[i].classList.toggle("box-white")
+    }
 }
 
 for(i=0; i<boxes.length; i++){
     boxes[i].addEventListener("mouseenter", colorChange);
     boxes[i].addEventListener("mouseleave", colorChange);
 }
-    
-
-// document.getElementsByClassName("col-sm-4").addEventListener("mouseout", function() {
-//     document.getElementsByClassName("col-sm-4").style.backgroundColor = "red";
-// });
-
-// for(i=0; i<boxes.length; i++){
-// boxes[i].addEventListener("mouseenter", 
-//     function() {
-//         var colorChange = document.getElementsByClassName("col-sm-4");
-//         colorChange.classList.toggle("box-white");
-//       });
-    // function() {
-    //     document.getElementsByClassName("col-sm-4").style.backgroundColor = "white"; });
-    // boxes[i].addEventListener("mouseleave", function(){ document.getElementsByClassName("col-sm-4").style.backgroundColor = "black"; });
-    // }
 
 var newPara = document.createElement("p");
 newPara.innerHTML = "";
@@ -46,7 +21,7 @@ document.getElementById("box1").addEventListener("click",
     function() {
         document.getElementById("container_fluid").appendChild(newPara);
         newPara.innerHTML = "Oooh - so close, but no cigar";
-        showButton.classList.toggle("hidden-button");
+        showButton.classList.remove("hidden-button");
     }, false
 );
 
@@ -54,7 +29,7 @@ document.getElementById("box2").addEventListener("click",
     function () {
         document.getElementById("container_fluid").appendChild(newPara);
         newPara.innerHTML = "DING DING DING - We have a winner";
-        showButton.classList.toggle("hidden-button");
+        showButton.classList.remove("hidden-button");
     }, false
 );
 
@@ -62,6 +37,6 @@ document.getElementById("box3").addEventListener("click",
     function () {
         document.getElementById("container_fluid").appendChild(newPara);
         newPara.innerHTML = "Oops, butter luck next time";
-        showButton.classList.toggle("hidden-button");
+        showButton.classList.remove("hidden-button");
     }, false 
 );
